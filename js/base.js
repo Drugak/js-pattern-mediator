5092619bd53cf4bfb9c56fde26cd7979291200c2
Vitaliy Drugak <vitosdrugak92@gmail.com>
2015-03-18 10:28:07 +0200

    start write mediator pattern

---
+++ js/base.js
@@ -0,0 +1,5 @@
+/**
+ * Created by vitaliydrugak on 18.03.15.
+ */
+
+"use strict"
---
+++ js/mediator.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vitaliydrugak on 18.03.15.
+ */
+
+"use strict"
+
+var mediator = (function() {
+	var listen = function () {
+
+	};
+});
