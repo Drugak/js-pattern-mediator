3ac90454dd48b9682ebbd436552bff98f2a8ab3b
Vitaliy Drugak <vitosdrugak92@gmail.com>
2015-03-19 18:56:31 +0200

    work

--- js/mediator.js
+++ js/mediator.js
@@ -5,7 +5,38 @@
 "use strict"
 
 var mediator = (function() {
-	var listen = function () {
+	var listen = function (channel,fn) {
+		if(!mediator.channels[channel])
+			mediator.channels[channel] = [];
 
+		mediator.channels[channel].push({
+				context: this,
+				callback: fn
+			});
+
+		return this;
+	},
+
+	publish = function(channel) {
+		if (!mediator.channels[channel]) return false;
+		var args = Array.prototype.slice.call(arguments, 1);
+		for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
+			var subscription = mediator.channels[channel][i];
+			subscription.callback.apply(subscription.context, args);
+		}
+
+		return this;
+	}
+
+
+
+	return {
+		channels: {},
+		publish: publish,
+		listen: listen,
+		installTo: function(obj) {
+			obj.listen = listen;
+			obj.publish = publish;
+		}
 	};
-});
+}());
